9e82e2990502889ce0ce996f72245f511e9f972a
FultonLin <Flin5289@bths.edu>
2017-01-10 00:09:41 -0500

    Update Button.java

--- simonFulton/simon/Button.java
+++ simonFulton/simon/Button.java
@@ -2,18 +2,24 @@ package simon;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Polygon;
+import java.awt.RenderingHints;
 
 import guiPractice.components.Action;
 import guiPractice.components.Component;
 
 public class Button extends Component implements ButtonInterfaceFulton {
 
+	private static final int WIDTH = 50;
+	private static final int HEIGHT = 50;
 	private Color color;
 	private final Color DIM = new Color(105, 105, 105);
 	public Action action;
+	private boolean highlight;
+	private Color displayColor;
 	
-	public Button(int x, int y, int w, int h) {
-		super(x, y, w, h);
+	public Button() {
+		super(0, 0, WIDTH, HEIGHT);
 	}
 
 	@Override
@@ -23,46 +29,46 @@ public class Button extends Component implements ButtonInterfaceFulton {
 
 	@Override
 	public boolean isHovered(int x, int y) {
-		if(inBetween(x, getX(), getX() + getWidth()) && inBetween(y, getY(), getY() + getHeight()))
-			return true;
-		else 
-			return false;
-	}
-	
-	public boolean inBetween(int x, int y, int z){
-		if(x > y && x < z)
-			return true;
-		else 
-			return false;
+		return x > getX() && x < getX()+getWidth() && y > getY() && y<getY()+getHeight();
 	}
 
-	@Override
 	public void setColor(Color color) {
 		this.color = color;
 	}
-
-	@Override
+	
 	public void setAction(Action a) {
 		this.action = a;
-	}
-
-	@Override
-	public void highlight() {
-		this.color = color;
 		update();
 	}
 
-	@Override
+	public void highlight() {
+		if(color != null){
+			displayColor = color;
+		}
+		highlight = true;
+		update();
+	}
+
 	public void dim() {
 		this.color = DIM;
+		highlight = false;
 		update();
 	}
 
-	@Override
 	public void update(Graphics2D g) {
-		g.setColor(this.color);
-		g.drawOval(getX(), getY(), getWidth(), getHeight());
-		g.fillOval(getX(), getY(), getWidth(), getHeight());
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		if(displayColor != null){ 
+			g.setColor(displayColor);
+		}else{
+			g.setColor(Color.gray);
+		}
+		g.fillOval(0, 0, WIDTH, HEIGHT);
+		g.setColor(Color.black);
+		g.drawOval(0, 0, WIDTH-1, HEIGHT-1);
+		if(highlight){
+			g.setColor(Color.white);
+			Polygon p = new Polygon();
+		}
 	}
 
 }
